f72681628336c92aeeb67b025bd911897aeeb438
ignaa7 <ignaciofernandez678@gmail.com>
2023-09-29 11:15:13 +0200

    ejs

---
+++ Desarrollo de Interfaces/Ejercicio5/Actividad5/Actividad5.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Actividad5
+{
+    public class Actividad5
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static string devolverString(string[] vs)
+        {
+            string cadena = "";
+
+            foreach(string parteCadena in vs)
+            {
+                cadena += parteCadena;
+            }
+
+            return cadena;
+
+            throw new NotImplementedException();
+        }
+
+        public static string devolverStringOrdenado(string[] vs)
+        {
+            string cadena = "";
+
+            Array.Sort(vs);
+
+            foreach(string parteCadena in vs) cadena += parteCadena;
+
+            return cadena;
+
+            throw new NotImplementedException();
+        }
+
+        public static int contarCaracter(string[] vs, char v)
+        {
+            int cont = 0;
+
+            foreach(string parteCadena in vs)
+            {
+                if (parteCadena.Contains(v)) cont++;
+            }
+
+            return cont;
+
+            throw new NotImplementedException();
+        }
+
+        public static int[] sumarArrays(int[] vs1, int[] vs2)
+        {
+            List<int> sumas = new List<int>();
+
+            for (int i = 0; i < vs1.Length; i++)
+            {
+                sumas.Add(vs1[i] + vs2[i]);
+            }
+
+            return sumas.ToArray();
+
+            throw new NotImplementedException();
+        }
+
+        public static int[] multiplicarArrays(int[] vs1, int[] vs2)
+        {
+            List<int> multiplicaciones = new List<int>();
+
+            for (int i = 0; i < vs1.Length; i++)
+            {
+                multiplicaciones.Add(vs1[i] * vs2[i]);
+            }
+
+            return multiplicaciones.ToArray();
+
+            throw new NotImplementedException();
+        }
+
+        public static bool isPalindromo(string entrada)
+        {
+            string[] partesCadena = entrada.Split(" ");
+            string cadena = "";
+            
+            foreach (string parteCadena in partesCadena)
+            {
+                cadena += parteCadena;
+            }
+
+            cadena = cadena.ToLower().Trim();
+
+            string cadenaAlReves = "";
+
+            foreach (char caracter in cadena)
+            {
+                cadenaAlReves += caracter;
+            }
+
+            return cadena.Equals(cadenaAlReves);
+
+            throw new NotImplementedException();
+
+
+        }
+
+        public static int contarPalabras(string entrada)
+        {
+            List<string> palabras = new List<string>();
+            string[] palabrasArray = entrada.Split(" ");
+
+            foreach (string palabraArray in palabrasArray)
+            {
+                if (palabraArray != "") palabras.Add(palabraArray);
+            }
+
+            return palabras.Count;
+
+            throw new NotImplementedException();
+        }
+
+        public static int contarCaracteres(string entrada)
+        {
+            string[] partesCadena = entrada.Split(" ");
+            string cadena = "";
+
+            foreach (string parteCadena in partesCadena)
+            {
+                cadena += parteCadena;
+            }
+
+            return cadena.Count();
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Desarrollo de Interfaces/diccionarios/diccionarios/Program.cs
@@ -0,0 +1,13 @@
+﻿Dictionary<int, string> miDiccionario = new Dictionary<int, string>();
+
+miDiccionario.Add(0, "cero");
+miDiccionario.Add(1, "uno");
+miDiccionario.Add(2, "dos");
+miDiccionario.Add(3, "tres");
+
+var enumerator = miDiccionario.GetEnumerator();
+
+while (enumerator.MoveNext())
+{
+    Console.WriteLine(enumerator.Current);
+}
