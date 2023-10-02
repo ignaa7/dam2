64a1a133577a04b1dc943f8e256eb315acf3c37e
ignaa7 <ignaciofernandez678@gmail.com>
2023-10-02 12:47:02 +0200

    android saludo

--- AccesoADatos/AndroidStudioProjects/HolaMundo/app/src/main/java/com/example/holamundo/MainActivity.java
+++ AccesoADatos/AndroidStudioProjects/HolaMundo/app/src/main/java/com/example/holamundo/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,11 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSaludar2 = (Button) findViewById(R.id.btnSaludar2);
+
+        btnSaludar2.setOnClickListener(this::mostrarSaludo2);
     }
 
     public void mostrarSaludo(View view) {
         TextView txtMensaje = findViewById(R.id.txtSaludo);
 
         txtMensaje.setText("Hola buenas");
+    }
+
+    public void mostrarSaludo2(View view) {
+        TextView txtMensaje = findViewById(R.id.txtSaludo);
+
+        txtMensaje.setText("Hasta luego");
     }
 }
