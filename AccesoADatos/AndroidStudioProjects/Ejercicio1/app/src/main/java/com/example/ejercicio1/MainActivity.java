ddf9a6cd012a097e10f4568c344b57556d497cc4
ignaa7 <105316942+ignaa7@users.noreply.github.com>
2023-10-05 23:06:12 +0200

    android botones Listener

--- AccesoADatos/AndroidStudioProjects/Ejercicio1/app/src/main/java/com/example/ejercicio1/MainActivity.java
+++ AccesoADatos/AndroidStudioProjects/Ejercicio1/app/src/main/java/com/example/ejercicio1/MainActivity.java
@@ -8,19 +8,33 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private int numero = 0;
     private TextView txtNumero;
+    private int idBtnMas;
+    private int idBtnMenos;
+    private int idBtnReset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txtNumero = findViewById(R.id.txtNumero);
-        txtNumero.setText(0);
+        this.idBtnMas = findViewById(R.id.btnMas).getId();
+        this.idBtnMenos = findViewById(R.id.btnMenos).getId();
+        this.idBtnReset = findViewById(R.id.btnReset).getId();
+        this.txtNumero = findViewById(R.id.txtNumero);
+        this.txtNumero.setText("0");
+
+        findViewById(R.id.btnMas).setOnClickListener(this);
+        findViewById(R.id.btnMenos).setOnClickListener(this);
+        findViewById(R.id.btnReset).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        
+        int viewId = view.getId();
+        int numero = Integer.parseInt(txtNumero.getText().toString());
+
+        if (viewId == idBtnMas) txtNumero.setText(numero + 1 + "");
+        else if (viewId == idBtnMenos && numero > 0) txtNumero.setText(numero - 1 + "");
+        else if (viewId == idBtnReset) txtNumero.setText("0");
     }
 }
