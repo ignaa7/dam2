997d4d0445dd49f0ab750ffbfc442b87eb22dc4b
ignaa7 <ignaciofernandez678@gmail.com>
2023-10-09 11:12:21 +0200

    deint teoria extensiones

---
+++ Desarrollo de Interfaces/Extensiones/Extensiones/Extensiones.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extensiones
+{
+    internal static class Extensiones
+    {
+        internal static int ContarPalabras(this string cadena)
+        {
+            return cadena.Split(" ").Count();
+        }
+
+        internal static string ImprimirArray<T>(this List<T> array)
+        {
+            string cadena = "";
+
+            foreach (T item in array)
+            {
+                cadena += $"{item}, ";
+            }
+
+            cadena = cadena.Remove(cadena.Length-2, 2);
+
+            return cadena;
+        }
+    }
+}
---
+++ Desarrollo de Interfaces/Extensiones/Extensiones/Program.cs
@@ -0,0 +1,11 @@
+﻿using Extensiones;
+
+string cadena = "Hola mundo";
+Console.WriteLine(cadena.ContarPalabras());
+
+
+List<int> numeros = new List<int> { 1, 2, 3, 4, 5 };
+Console.WriteLine(numeros.ImprimirArray());
+
+List<string> palabras = new List<string> { "primero", "segundo", "tercero", "cuarto", "quinto" };
+Console.WriteLine(palabras.ImprimirArray());
