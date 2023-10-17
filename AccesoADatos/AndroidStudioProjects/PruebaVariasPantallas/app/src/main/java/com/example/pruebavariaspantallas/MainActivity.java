2ae37d9ee71ef2831b4bb6fef135caad386d6d96
ignaa7 <ignaciofernandez678@gmail.com>
2023-10-17 13:53:20 +0200

    acceso a datos prueba varias pantallas

---
+++ AccesoADatos/AndroidStudioProjects/PruebaVariasPantallas/app/src/main/java/com/example/pruebavariaspantallas/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.pruebavariaspantallas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(this, MainActivity2.class);
+        startActivity(i);
+    }
+}
---
+++ AccesoADatos/AndroidStudioProjects/PruebaVariasPantallas/app/src/main/java/com/example/pruebavariaspantallas/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.example.pruebavariaspantallas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    @Override
+    public void onClick(View v) {
+        finish();
+    }
+}
