d12a77761640f868bb320f39b94b2f167850b819
ignaa7 <ignaciofernandez678@gmail.com>
2023-10-19 14:44:53 +0200

    angular create header project

---
+++ Programacion Multimedia/Angular/2-PaginaCabecera/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponentComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
