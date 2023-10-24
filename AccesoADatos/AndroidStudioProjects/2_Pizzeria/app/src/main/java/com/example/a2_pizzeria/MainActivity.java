c498f5420e23a974a9870b27e958bdbb8efbf7da
ignaa7 <ignaciofernandez678@gmail.com>
2023-10-24 14:02:10 +0200

    android pizzeria signUpUsers incompleto

--- AccesoADatos/AndroidStudioProjects/2_Pizzeria/app/src/main/java/com/example/a2_pizzeria/MainActivity.java
+++ AccesoADatos/AndroidStudioProjects/2_Pizzeria/app/src/main/java/com/example/a2_pizzeria/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.a2_pizzeria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.a2_pizzeria.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences preferences = getSharedPreferences("users", MODE_PRIVATE); //error
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+
+    public void signUpUser() {
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(binding.edtName.getText().toString(), binding.edtPassword.getText().toString());
     }
 }
