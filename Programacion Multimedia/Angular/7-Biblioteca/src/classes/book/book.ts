7c3d065eb7fe30a06fc4d248156f5d8830d6cd69
ignaa7 <ignaciofernandez678@gmail.com>
2023-11-16 14:33:51 +0100

    -

--- Programacion Multimedia/Angular/7-Biblioteca/src/classes/book/book.ts
+++ Programacion Multimedia/Angular/7-Biblioteca/src/classes/book/book.ts
@@ -1,2 +1,29 @@
 export class Book {
+    private _title: string;
+    private _author: string;
+    private _isbn: string;
+    private _date: Date;
+
+    constructor(title: string, author: string, isbn: string, date: Date) {
+        this._title = title;
+        this._author = author;
+        this._isbn = isbn;
+        this._date = date;
+    }
+
+    public get title(): string {
+        return this._title;
+    }
+
+    public get author(): string {
+        return this._author;
+    }
+
+    public get isbn(): string {
+        return this._isbn;
+    }
+
+    public get date(): Date {
+        return this._date;
+    }
 }
