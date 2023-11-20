9103a990a2782c611cc717a045583c3c2b01da09
ignaa7 <ignaciofernandez678@gmail.com>
2023-11-20 11:14:17 +0100

    deint ej8 creation

---
+++ Desarrollo de Interfaces/Tema3/Ejercicio8/Ejercicio8/Banco.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio8
+{
+    internal class Banco
+    {
+        public List<Cliente> Clientes { get; set; }
+
+        public Banco()
+        {
+            
+        }
+
+        public bool agregarCliente(string dni, string nombre, string direccion,  int edad, string telefono, string numeroCuentaCorriente)
+        {
+            bool correcto = false;
+
+            return correcto;
+        }
+    }
+}
---
+++ Desarrollo de Interfaces/Tema3/Ejercicio8/Ejercicio8/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio8
+{
+    internal class Cliente
+    {
+        public string Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public int Edad { get; set; }
+        public string Telefono { get; set; }
+        public string NumeroCuentaCorriente { get; set; }
+    }
+}
