609936e55b98c388574ec4a7235536f3e848156e
ignaa7 <ignaciofernandez678@gmail.com>
2023-11-21 13:01:21 +0100

    deint ej8 not finished

--- Desarrollo de Interfaces/Tema3/Ejercicio8/Ejercicio8/Cliente.cs
+++ Desarrollo de Interfaces/Tema3/Ejercicio8/Ejercicio8/Cliente.cs
@@ -14,5 +14,20 @@ namespace Ejercicio8
         public int Edad { get; set; }
         public string Telefono { get; set; }
         public string NumeroCuentaCorriente { get; set; }
+
+        public Cliente(string dni, string nombre, string direccion, int edad, string telefono, string numeroCuentaCorriente)
+        {
+            this.Dni = dni;
+            this.Nombre = nombre;
+            this.Direccion = direccion;
+            this.Edad = edad;
+            this.Telefono = telefono;
+            this.NumeroCuentaCorriente = numeroCuentaCorriente;
+        }
+
+        public override string ToString()
+        {
+            return $"DNI: {Dni}\nNombre: {Nombre}\nDirección: {Direccion}\nEdad: {Edad}\nTeléfono: {Telefono}\nNumero de cuenta corriente: {NumeroCuentaCorriente}";
+        }
     }
 }
