7e8f5b158c425cd9d4e7c0490001ba2bf60861db
ignaa7 <ignaciofernandez678@gmail.com>
2023-11-28 10:59:49 +0100

    -

---
+++ AccesoADatos/lambdas/src/lambdas/InterfazFuncionalMayor.java
@@ -0,0 +1,6 @@
+package lambdas;
+
+@FunctionalInterface
+public interface InterfazFuncionalMayor {
+	int getMayor(int num1, int num2, int num3);
+}
--- AccesoADatos/lambdas/src/lambdas/Main.java
+++ AccesoADatos/lambdas/src/lambdas/Main.java
@@ -44,19 +44,98 @@ public class Main {
 		// 4
 		InterfazFuncional numeroMayor = (int... numeros) -> {
 			if (numeros.length == 0) {
-                return 0;
+                return null;
             }
 
-            int suma = 0;
+            int mayor = numeros[0];
             
             for (int numero : numeros) {
-                suma += numero;
+                if (numero > mayor) mayor = numero;
             }
 
-            return (double) suma / numeros.length;
+            return mayor;
 		};
 		
-		System.out.println(mediaEnteros.calcular(1, 2, 3, 4));
+		Object mayor = numeroMayor.calcular();
+		
+		if (mayor == null) {
+			System.out.println("No ha introducido ningún número");
+		} else {
+			System.out.println(mayor);
+		}
+		
+		
+		// 5
+		InterfazFuncional numeroMenor = (int... numeros) -> {
+			if (numeros.length == 0) {
+                return null;
+            }
+
+            int menor = numeros[0];
+            
+            for (int numero : numeros) {
+                if (numero < menor) menor = numero;
+            }
+
+            return menor;
+		};
+		
+		Object menor = numeroMayor.calcular();
+		
+		if (menor == null) {
+			System.out.println("No ha introducido ningún número");
+		} else {
+			System.out.println(menor);
+		}
+		
+		
+		// 6
+		InterfazFuncional moda = (int... numeros) -> {
+			if (numeros.length == 0) {
+                return null;
+            }
+
+            int numeroModa = 0;
+            int contNumeroModa = 0;
+            
+            for (int numero : numeros) {
+                int cont = 0;
+                
+                for (int numeroBucle : numeros) {
+                	if (numeroBucle == numero) cont++;
+                }
+                
+                if (cont > contNumeroModa) {
+                	numeroModa = numero;
+                	contNumeroModa = cont;
+                }
+            }
+
+            return numeroModa;
+		};
+		
+		Object numeroModa = moda.calcular();
+		
+		if (numeroModa == null) {
+			System.out.println("No ha introducido ningún número");
+		} else {
+			System.out.println(moda);
+		}
+		
+		
+		// 7
+		InterfazFuncionalMayor numeroMayorEntre3 = (int num1, int num2, int num3) -> {
+			int numMayor = num1;			
+            
+            if (num2 > numMayor) numMayor = num2;
+            if (num3 > numMayor) numMayor = num3;
+
+            return numMayor;
+		};
+		
+		int numMayor = numeroMayorEntre3(1, 2, 3);
+		
+		System.out.println(numMayor);
 	}
 
 }
