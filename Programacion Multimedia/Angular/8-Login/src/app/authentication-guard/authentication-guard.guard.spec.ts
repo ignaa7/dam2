a445ea6834c37d795e9b2c9ba4d48bbed9455db8
ignaa7 <105316942+ignaa7@users.noreply.github.com>
2023-12-01 00:58:52 +0100

    -

--- Programacion Multimedia/Angular/8-Login/src/app/authentication-guard/authentication-guard.guard.spec.ts
+++ Programacion Multimedia/Angular/8-Login/src/app/authentication-guard/authentication-guard.guard.spec.ts
@@ -1,11 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { CanActivateFn } from '@angular/router';
 
-import { authenticationGuardGuard } from './authentication-guard.guard';
+import { authenticationGuard } from './authentication-guard.guard';
 
 describe('authenticationGuardGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authenticationGuardGuard(...guardParameters));
+      TestBed.runInInjectionContext(() => authenticationGuard(...guardParameters));
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
