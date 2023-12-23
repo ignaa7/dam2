ca679a63a2d7d6d648670d6abb3c1ca1c5fd339c
ignaa7 <105316942+ignaa7@users.noreply.github.com>
2023-12-23 18:32:52 +0100

    deint maui ejercicio 2 terminado

---
+++ Desarrollo de Interfaces/Tema4Maui/Ejercicio2/Ejercicio2/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿namespace Ejercicio2
+{
+    public partial class MainPage : ContentPage
+    {
+        double propina = 0;
+        int numeroPersonas = 1;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void calcularPrecio()
+        {
+            double cuenta;
+
+            if (double.TryParse(entryCuenta.Text, out cuenta))
+            {
+                double subtotal = cuenta / numeroPersonas;
+                lblSubtotal.Text = $"{subtotal}€";
+
+                lblPropina.Text = $"{subtotal * propina / 100}€";
+
+                lblTotal.Text = $"{subtotal * (propina / 100 + 1)}€";
+            }
+        }
+
+        private void entryCuenta_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            calcularPrecio();
+        }
+
+        private void btnPropina10_Clicked(object sender, EventArgs e)
+        {
+            propina = 10;
+            sliderPropina.Value = propina;
+            calcularPrecio();
+        }
+
+        private void btnPropina20_Clicked(object sender, EventArgs e)
+        {
+            propina = 20;
+            sliderPropina.Value = propina;
+            calcularPrecio();
+        }
+
+        private void btnPropina30_Clicked(object sender, EventArgs e)
+        {
+            propina = 30;
+            sliderPropina.Value = propina;
+            calcularPrecio();
+        }
+
+        private void sliderPropina_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            propina = sliderPropina.Value;
+            calcularPrecio();
+        }
+
+        private void btnRestarNumeroPersonas_Clicked(object sender, EventArgs e)
+        {
+            if (numeroPersonas > 0)
+            {
+                numeroPersonas--;
+                lblNumeroPersonas.Text = numeroPersonas.ToString();
+                calcularPrecio();
+            }
+        }
+
+        private void btnSumarNumeroPersonas_Clicked(object sender, EventArgs e)
+        {
+            numeroPersonas++;
+            lblNumeroPersonas.Text = numeroPersonas.ToString();
+            calcularPrecio();
+        }
+    }
+}
