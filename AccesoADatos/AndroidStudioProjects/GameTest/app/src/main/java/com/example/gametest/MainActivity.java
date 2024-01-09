d7c1ee40609d47789f9daab87cf702342819419d
ignaa7 <ignaciofernandez678@gmail.com>
2024-01-09 23:02:37 +0100

    -

--- AccesoADatos/AndroidStudioProjects/GameTest/app/src/main/java/com/example/gametest/MainActivity.java
+++ AccesoADatos/AndroidStudioProjects/GameTest/app/src/main/java/com/example/gametest/MainActivity.java
@@ -8,56 +8,69 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends Activity implements View.OnTouchListener {
 
     TextView textView;
+    StringBuilder builder = new StringBuilder();
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        StringBuilder builder = new StringBuilder();
+        builder.setLength(0);
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                builder.setLength(0);
-                builder.append("X: ");
-                builder.append(event.getX());
-                builder.append("\nY: ");
-                builder.append(event.getY());
-                textView.setText(builder);
+                builder.append("Has pulsado.\n");
+                break;
             case MotionEvent.ACTION_MOVE:
-                builder.setLength(0);
-                builder.append("X: ");
-                builder.append(event.getX());
-                builder.append("\nY: ");
-                builder.append(event.getY());
-                textView.setText(builder);
+                builder.append("Estás moviendo.\n");
+                break;
             case MotionEvent.ACTION_CANCEL:
-                builder.setLength(0);
-                builder.append("X: ");
-                builder.append(event.getX());
-                builder.append("\nY: ");
-                builder.append(event.getY());
-                textView.setText(builder);
+                builder.append("Has cancelado.\n");
+                break;
             case MotionEvent.ACTION_UP:
-                builder.setLength(0);
-                builder.append("X: ");
-                builder.append(event.getX());
-                builder.append("\nY: ");
-                builder.append(event.getY());
-                textView.setText(builder);
+                builder.append("Has subido.\n");
+                break;
         }
+        builder.append("X: ");
+        builder.append(event.getX());
+        builder.append("\nY: ");
+        builder.append(event.getY());
+        textView.setText(builder);
+
         return true;
     }
+
+    /*private void updateCoordinates() {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText(builder);
+                    }
+                });
+            }
+        });
+    }*/
 
     class RenderView extends View {
         Random random = new Random();
@@ -65,6 +78,9 @@ public class MainActivity extends Activity implements View.OnTouchListener {
 
         public RenderView(Context context) {
             super(context);
+            textView = new TextView(context);
+            textView.setTextColor(Color.BLACK);
+            textView.setTextSize(20);
         }
 
         protected  void onDraw(Canvas canvas) {
@@ -92,13 +108,16 @@ public class MainActivity extends Activity implements View.OnTouchListener {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        textView.setText("Desliza el dedo");
-        textView.setOnTouchListener(this);
-        setContentView(new RenderView(this));
 
-        /*textView = new TextView(this);
+        FrameLayout frameLayout = new FrameLayout(this);
+        RenderView renderView = new RenderView(this);
+        frameLayout.addView(renderView);
+
+        textView = new TextView(this);
         textView.setText("Desliza el dedo");
         textView.setOnTouchListener(this);
-        setContentView(textView);*/
+        frameLayout.addView(textView);
+
+        setContentView(frameLayout);
     }
 }
