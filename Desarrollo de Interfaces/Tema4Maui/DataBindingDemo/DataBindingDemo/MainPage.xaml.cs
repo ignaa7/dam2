2ec7d1b7266b9fcd26eaf1b7f153ece07b9915e9
ignaa7 <ignaciofernandez678@gmail.com>
2024-01-10 12:44:18 +0100

    deint dataBindingDemo

---
+++ Desarrollo de Interfaces/Tema4Maui/DataBindingDemo/DataBindingDemo/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using DataBindingDemo.Models;
+
+namespace DataBindingDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            var person = new Person
+            {
+                Name = "Pepe",
+                Address = "Larga",
+                Phone = "123456789"
+            };
+
+            BindingContext = person;
+        }
+    }
+}
