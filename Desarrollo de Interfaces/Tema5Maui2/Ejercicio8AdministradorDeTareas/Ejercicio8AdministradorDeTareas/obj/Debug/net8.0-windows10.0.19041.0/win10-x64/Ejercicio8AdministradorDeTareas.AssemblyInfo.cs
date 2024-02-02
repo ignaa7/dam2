170bdb423e83608c1e160787f50c16f25adc1c70
ignaa7 <ignaciofernandez678@gmail.com>
2024-02-02 14:38:07 +0100

    -

--- Desarrollo de Interfaces/Tema5Maui2/Ejercicio8AdministradorDeTareas/Ejercicio8AdministradorDeTareas/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Ejercicio8AdministradorDeTareas.AssemblyInfo.cs
+++ Desarrollo de Interfaces/Tema5Maui2/Ejercicio8AdministradorDeTareas/Ejercicio8AdministradorDeTareas/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/Ejercicio8AdministradorDeTareas.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio8AdministradorDeTareas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5f0cd587ffa76118a9ea290a87e48cfc5371a7a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+7c12c08395343cc3767c1d422a3a90bb33a531e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio8AdministradorDeTareas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio8AdministradorDeTareas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Programacion Multimedia/ionic/BookingApp/src/app/app-routing.module.ts
+++ Programacion Multimedia/ionic/BookingApp/src/app/app-routing.module.ts
@@ -3,14 +3,24 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'main',
+    loadChildren: () => import('./main-page/main/main.module').then( m => m.MainPageModule)
+  },
 ];
+
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
